50cd122cc7952cb2b573bbfe880067eed2410073
PamelaShanelly <Pamelapayanocaceres@gmail.com>
2026-05-06 08:57:09 -0400

    Add files via upload

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+// Test Connection
+async function testConnection() {
+  try {
+    // We use a dummy doc to test connection
+    await getDocFromServer(doc(db, '_connection_test_', 'ping'));
+    console.log("Firebase connection established successfully.");
+  } catch (error) {
+    if (error instanceof Error && error.message.includes('the client is offline')) {
+      console.error("Please check your Firebase configuration or internet connection.");
+    }
+  }
+}
+
+testConnection();
